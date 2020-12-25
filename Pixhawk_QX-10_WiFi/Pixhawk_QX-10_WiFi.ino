c7e2cfb75d179eae5c20fc6cd0ad0dd0590f6cc4
rizkymille <66122204+rizkymille@users.noreply.github.com>
2020-12-25 18:02:36 +0700

    Add files via upload

---
+++ Pixhawk_QX-10_WiFi/Pixhawk_QX-10_WiFi.ino
@@ -0,0 +1,118 @@
+/* credits to Andrej for the Sony QX-10 remote
+ * source: https://github.com/kuczy/Sony-QX-10-WiFi-Remote-Control-ESP8266-Arduino/tree/master/Sony_QX-10_Remote
+ */
+
+#include <WiFi.h>
+//#include <ArduinoJson.h>
+
+//variables kept in flash memory
+const char* StillMode PROGMEM = "{\"method\":\"setShootMode\",\"params\":[\"still\"],\"id\":1,\"version\":\"1.0\"}";
+const char* HalfPressShutter PROGMEM = "{\"method\":\"actHalfPressShutter\",\"params\":[],\"id\":1,\"version\":\"1.0\"}";
+const char* TakePicture PROGMEM = "{\"method\":\"actTakePicture\",\"params\":[],\"id\":1,\"version\":\"1.0\"}";
+const char* MovieMode PROGMEM = "{\"method\":\"setShootMode\",\"params\":[\"movie\"],\"id\":1,\"version\":\"1.0\"}";
+const char* StartMovie PROGMEM = "{\"method\":\"startMovieRec\",\"params\":[],\"id\":1,\"version\":\"1.0\"}";
+const char* StopMovie PROGMEM = "{\"method\":\"stopMovieRec\",\"params\":[],\"id\":1,\"version\":\"1.0\"}";
+const char* ssid PROGMEM     = "DIRECT-dUQ0:DSC-QX10";
+const char* password PROGMEM = "NE8oHWt5";
+const char* host PROGMEM = "10.0.0.1";
+const int httpPort PROGMEM = 10000;
+//camera trigger pin
+const int camtrig = 13;
+int camtrig_value;
+
+//global variable
+WiFiClient client;
+
+//prototypes function
+void sendCommandToCamera(String command);
+
+//initialization
+void setup() {
+  //reading pwm trigger from pixhawk
+  pinMode(camtrig, INPUT);
+  
+  Serial.begin(115200);
+  delay(1000);
+
+  //start by connecting to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to WiFi network: ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi Connection Active"); 
+  Serial.println("IP Address: ");
+  Serial.println(WiFi.localIP());
+ 
+  //-----------------------
+  delay(2000);
+
+  /*FPSTR does const char* String, Read: https://github.com/esp8266/Arduino/blob/master/doc/reference.md#progmem
+  sendCommandToCamera(FPSTR(TakePicture));
+  /*
+  delay(500);
+   char c[1024];
+  // Read all the lines of the reply from server and print them to Serial
+   while(client.available()){
+   c[0] = client.read();
+   Serial.print(c);
+   }
+   
+   StaticJsonBuffer<200> jsonBuffer;
+   JsonObject& root = jsonBuffer.parseObject(c);
+  int data = root["lowVersion"]; 
+  Serial.println();
+     Serial.print(data);
+     
+  Serial.println();
+  Serial.println("closing connection"); 
+  */
+}
+
+//main loop
+void loop(){
+  camtrig_value = pulseIn(camtrig, HIGH);
+  if (camtrig_value > 1500){
+    delay(100);
+    sendCommandToCamera(HalfPressShutter);
+    sendCommandToCamera(TakePicture);
+  }
+}
+
+//function declarations
+void sendCommandToCamera(String command){
+  Serial.print("Linking to an adress ");
+  Serial.println(host);
+  if (!client.connect(host, httpPort)) {
+    Serial.println("Connection failed");
+    return;
+  }
+  
+  Serial.print("Send a JSON inquiry ");
+  //this will send the request to the server
+  client.println("POST /sony/camera HTTP/1.1");
+  client.println("Connection: close");
+  client.println("Content-Type: application/json;charset=utf-8");
+  client.print("Content-Length: ");
+  client.println(command.length());
+  client.println();
+  client.println(command);
+
+  //wait for the third answer
+  delay(500);
+
+  while(client.available()){
+    Serial.print(client.read());
+  }
+
+  //close the connection
+  client.stop();
+}
